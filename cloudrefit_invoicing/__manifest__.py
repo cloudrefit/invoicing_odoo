f70a1dfd2875d70a1aaaeedca991f026b3301667
Dr. Mohammed AlSallout <mhd2111@gmail.com>
2026-06-08 13:39:05 +0300

    refactor: simplify versioning logic

--- cloudrefit_invoicing/__manifest__.py
+++ cloudrefit_invoicing/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CloudRefit ZATCA Gateway',
-    'version': '19.0.0.4',
+    'version': '19.0.0.5',
     'category': 'Accounting',
     'summary': 'بوابة كلاود ريفيت ZATCA',
     'description': """
