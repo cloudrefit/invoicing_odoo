16138f9a970ea29dd4bfecd8d0ae59bcb76da6c7
Dr. Mohammed AlSallout <mhd2111@gmail.com>
2026-06-08 13:39:09 +0300

    chore: move documentation out of repo to platform docs

--- cloudrefit_invoicing/__manifest__.py
+++ cloudrefit_invoicing/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CloudRefit ZATCA Gateway',
-    'version': '19.0.0.13',
+    'version': '19.0.0.14',
     'category': 'Accounting',
     'summary': 'بوابة كلاود ريفيت ZATCA',
     'description': """
