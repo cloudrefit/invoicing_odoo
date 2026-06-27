19d939ce3e2f2415bf039df01dbae3e6f9ae8b61
Dr. Mohammed AlSallout <mhd2111@gmail.com>
2026-06-27 14:08:48 +0300

    ui: rename sandbox button to 'Push to ZATCA Sandbox'
    Bump version 19.0.0.49 -> 19.0.0.50

--- cloudrefit_invoicing/__manifest__.py
+++ cloudrefit_invoicing/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CloudRefit ZATCA Gateway',
-    'version': '19.0.0.49',
+    'version': '19.0.0.50',
     'category': 'Accounting',
     'summary': 'بوابة كلاود ريفيت ZATCA',
     'description': """
