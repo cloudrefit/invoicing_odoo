69f3ef3c9420b8aeca5a1a7f78966560f11cbe89
Dr. Mohammed AlSallout <mhd2111@gmail.com>
2026-06-28 19:19:35 +0300

    feat: migrated ZATCA statuses from ribbons to alert banners to prevent hiding native Odoo Paid ribbon

--- cloudrefit_invoicing/__manifest__.py
+++ cloudrefit_invoicing/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CloudRefit ZATCA Gateway',
-    'version': '19.0.0.67',
+    'version': '19.0.0.68',
     'category': 'Accounting',
     'summary': 'بوابة كلاود ريفيت ZATCA',
     'description': """
