ed0b08a31b3007601c55d876ff5731913415aefc
Dr. Mohammed AlSallout <mhd2111@gmail.com>
2026-06-28 19:22:51 +0300

    feat: restored ZATCA ribbons with custom CSS positioning them perfectly in the top-left corner to accommodate native Odoo ribbons

--- cloudrefit_invoicing/__manifest__.py
+++ cloudrefit_invoicing/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CloudRefit ZATCA Gateway',
-    'version': '19.0.0.68',
+    'version': '19.0.0.69',
     'category': 'Accounting',
     'summary': 'بوابة كلاود ريفيت ZATCA',
     'description': """
