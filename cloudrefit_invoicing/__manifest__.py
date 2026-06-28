c8487ef0031ace7b35d7a864b635ee1fa9c53803
Dr. Mohammed AlSallout <mhd2111@gmail.com>
2026-06-28 17:07:55 +0300

    fix: changed window action target from inline to current to resolve RPC error

--- cloudrefit_invoicing/__manifest__.py
+++ cloudrefit_invoicing/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CloudRefit ZATCA Gateway',
-    'version': '19.0.0.64',
+    'version': '19.0.0.65',
     'category': 'Accounting',
     'summary': 'بوابة كلاود ريفيت ZATCA',
     'description': """
