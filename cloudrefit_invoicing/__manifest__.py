2bf48c534862dd100922b74d9e89f18a4491f909
Dr. Mohammed AlSallout <mhd2111@gmail.com>
2026-07-15 14:19:25 +0300

    [v19.0.0.142] style: move payment integration block below live settings

--- cloudrefit_invoicing/__manifest__.py
+++ cloudrefit_invoicing/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CloudRefit ZATCA Gateway',
-    'version': '19.0.0.141',
+    'version': '19.0.0.142',
     'category': 'Accounting',
     'summary': 'بوابة كلاود ريفيت ZATCA',
     'description': """
