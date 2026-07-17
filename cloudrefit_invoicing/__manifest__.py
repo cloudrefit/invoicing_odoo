d2059588280187503dac9a3bfc303b5d7bac0263
Dr. Mohammed AlSallout <mhd2111@gmail.com>
2026-07-18 01:47:08 +0300

    chore: update RELEASE.json for version 19.0.0.149

--- cloudrefit_invoicing/__manifest__.py
+++ cloudrefit_invoicing/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CloudRefit ZATCA Gateway',
-    'version': '19.0.0.149',
+    'version': '19.0.0.150',
     'category': 'Accounting',
     'summary': 'بوابة كلاود ريفيت ZATCA',
     'description': """
