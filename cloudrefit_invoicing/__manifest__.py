0989f874de5ba3f83496e16ba9cfd485beb7c4d5
Dr. Mohammed AlSallout <mhd2111@gmail.com>
2026-07-19 17:27:40 +0300

    fix: escape ampersand in report_invoice.xml label

--- cloudrefit_invoicing/__manifest__.py
+++ cloudrefit_invoicing/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CloudRefit ZATCA Gateway',
-    'version': '19.0.0.152',
+    'version': '19.0.0.153',
     'category': 'Accounting',
     'summary': 'بوابة كلاود ريفيت ZATCA',
     'description': """
