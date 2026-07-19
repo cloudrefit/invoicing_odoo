a47a8757cebff146df3a9408b46120da4f011d6e
Dr. Mohammed AlSallout <mhd2111@gmail.com>
2026-07-19 20:15:50 +0300

    fix: remove null

--- cloudrefit_invoicing/__manifest__.py
+++ cloudrefit_invoicing/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CloudRefit ZATCA Gateway',
-    'version': '19.0.0.154',
+    'version': '19.0.0.155',
     'category': 'Accounting',
     'summary': 'بوابة كلاود ريفيت ZATCA',
     'description': """
